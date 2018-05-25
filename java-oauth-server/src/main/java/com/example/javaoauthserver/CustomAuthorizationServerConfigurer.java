b4a4a0e668c821bb69be2416ead1417cf585633c
Vorrarit Luengwattanakij <vorrarit@gmail.com>
2018-05-25 13:24:49 +0700

    simple authorization server - add grant_type password

--- java-oauth-server/src/main/java/com/example/javaoauthserver/CustomAuthorizationServerConfigurer.java
+++ java-oauth-server/src/main/java/com/example/javaoauthserver/CustomAuthorizationServerConfigurer.java
@@ -1,11 +1,17 @@
 package com.example.javaoauthserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 public class CustomAuthorizationServerConfigurer extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -14,5 +20,10 @@ public class CustomAuthorizationServerConfigurer extends AuthorizationServerConf
 				.authorizedGrantTypes("password", "client_credentials", "authorization_code", "refresh_token")
 				.secret("{noop}secret")
 				.scopes("all");
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
 	}
 }
---
+++ java-oauth-server/src/main/java/com/example/javaoauthserver/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.javaoauthserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return authenticationManager();
+	}
+	
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("user1")
+				.password("password")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+}
