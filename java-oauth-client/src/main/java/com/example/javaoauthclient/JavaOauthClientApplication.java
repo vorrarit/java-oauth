9f93c432c1b152ae59b7e1e8823884d9133e5ee5
Vorrarit Luengwattanakij <vorrarit@gmail.com>
2018-05-25 18:28:18 +0700

    enable home page and local rest api call

--- java-oauth-client/src/main/java/com/example/javaoauthclient/JavaOauthClientApplication.java
+++ java-oauth-client/src/main/java/com/example/javaoauthclient/JavaOauthClientApplication.java
@@ -1,19 +1,43 @@
 package com.example.javaoauthclient;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class JavaOauthClientApplication {
+@RestController
+public class JavaOauthClientApplication extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	OAuth2ClientContext oauth2ClientContext;
 
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**")
+				.authorizeRequests()
+			.antMatchers("/", "/login**", "/webjars/**", "/error**")
+				.permitAll()
+			.anyRequest().authenticated()
+			.and().logout().logoutSuccessUrl("/").permitAll()
+			.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaOauthClientApplication.class, args);
 	}
